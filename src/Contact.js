6dec65bde720b1ebffde6990fe5b776c024aba3d
Johnny Nolan <i.am@jnoweb.com>
2019-04-20 13:07:14 +0200

    reintroducing eslint

--- src/Contact.js
+++ src/Contact.js
@@ -13,7 +13,6 @@ class Contact extends React.Component {
   authorize(e) {
     const password = e.target.querySelector(
       'input[type="password"]').value;
-    // eslint-disable-next-line
     const auth = password == this.state.password;
     this.setState({
       authorized: auth
